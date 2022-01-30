c66f00c560542f8d59ad1aa1d95b84fbacd53b12
Aydın Terzi <aydinterzi7@gmail.com>
2022-01-31 01:13:12 +0300

    heroku3

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/surchat'));
+app.use(express.static('./docs/surchat'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/surchat/'}),
+    res.sendFile('index.html', {root: 'docs/surchat/'}),
 );
 
 // Start the app by listening on the default Heroku port
